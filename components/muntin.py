b0b0fceb812fe2484fc37f5d5adf6dbfa6916caa
Abhishek M C <atlasmonu@gmail.com>
2024-04-09 19:36:10 +0530

    Added support for two axis muntin placement

--- components/muntin.py
+++ components/muntin.py
@@ -6,27 +6,63 @@ class Muntin:
     def __init__(self, panel_object):
         self.panel_object = panel_object
 
-    def draw_line(self, points):
+    def draw_line(self, points, thickness=None):
         """
         Args: points - ((x,y),(x1,y1)) - tuple of points to draw line bw
 
         """
-        self.panel_object.context.move_to(points[0][0], points[0][1])
-        self.panel_object.context.line_to(points[1][0], points[1][1])
-        self.panel_object.context.stroke()
+
+        if thickness:
+            # draw as a rectangle
+            x1, y1 = points[0][0], points[0][1]
+            x2, y2 = points[1][0], points[1][1]
+
+            width = thickness * self.panel_object.scale_factor
+            height = abs(y2 - y1)
+
+            x = min(x1, x2)
+            y = min(y1, y2)
+
+            if height == 0:
+                # its a horizontal line
+                height = width
+                width = abs(x2 - x1)
+
+            self.panel_object.context.rectangle(x, y, width, height)
+            self.panel_object.context.fill()
+        else:
+            # draw normal line
+            self.panel_object.context.move_to(points[0][0], points[0][1])
+            self.panel_object.context.line_to(points[1][0], points[1][1])
+            self.panel_object.context.stroke()
 
     def draw_muntin_part_from_placements(self, part_data, x, y):
         for position in part_data['placement_positions']:
-            scaled_start = position * self.panel_object.scale_factor
-            scaled_part_length = part_data['length'] * self.panel_object.scale_factor
 
-            if part_data['orientation'] == 'vertical':
-                start_point = (x + scaled_start, y)
-                end_point = (x + scaled_start, y + scaled_part_length)
+            if isinstance(position, (float, int)):
+                scaled_start = position * self.panel_object.scale_factor
+                scaled_part_length = part_data['length'] * self.panel_object.scale_factor
+
+                if part_data['orientation'] == 'vertical':
+                    start_point = (x + scaled_start, y)
+                    end_point = (x + scaled_start, y + scaled_part_length)
+                else:
+                    start_point = (x, y + scaled_start)
+                    end_point = (x + scaled_part_length, y + scaled_start)
             else:
-                start_point = (x, y + scaled_start)
-                end_point = (x + scaled_part_length, y + scaled_start)
-            self.draw_line((start_point, end_point))
+                scaled_start_axis_1 = position[0] * self.panel_object.scale_factor
+                scaled_start_axis_2 = position[1] * self.panel_object.scale_factor
+
+                scaled_part_length = part_data['length'] * self.panel_object.scale_factor
+
+                if part_data['orientation'] == 'vertical':
+                    start_point = (x + scaled_start_axis_1, y + scaled_start_axis_2)
+                    end_point = (x + scaled_start_axis_1, y + scaled_start_axis_2 + scaled_part_length)
+                else:
+                    start_point = (x + scaled_start_axis_2, y + scaled_start_axis_1)
+                    end_point = (x + scaled_start_axis_2 + scaled_part_length, y + scaled_start_axis_1)
+
+            self.draw_line((start_point, end_point), thickness=part_data.get('thickness', None))
 
     def draw_muntin(self):
         muntin_parameters = self.panel_object.muntin_parameters
@@ -43,6 +79,9 @@ class Muntin:
 
         x = self.panel_object.x + dlo_x_offset
         y = self.panel_object.y + dlo_y_offset
+
+        self.dlo_min_x = x
+        self.dlo_min_y = y
 
         b_offset = 0.10 * dlo_width
 
